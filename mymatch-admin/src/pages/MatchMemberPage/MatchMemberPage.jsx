79935f5fddc77bf3aa128fa5aa647bae5ccabf21
NguyenSiVanHao <HaoNSVSE172181@fpt.edu.vn>
2025-11-10 00:09:34 +0700

    [MM-ADMIN-0005] Match member page

--- mymatch-admin/src/pages/MatchMemberPage/MatchMemberPage.jsx
+++ mymatch-admin/src/pages/MatchMemberPage/MatchMemberPage.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Tabs } from 'antd'
+import { TeamOutlined, UserOutlined } from '@ant-design/icons'
+import Group from './Group'
+import Member from './Member'
+
+const { TabPane } = Tabs
 
 function MatchMemberPage() {
   return (
-    <div>MatchMemberPage</div>
+    <div style={{ padding: '24px', background: '#fff' }}>
+      <h2 style={{ marginBottom: '24px' }}>Swap Request Management</h2>
+        <Tabs
+          defaultActiveKey="1"
+          type="card"
+          tabBarGutter={32}
+          size="large"
+        >
+          <TabPane
+            tab={
+              <span>
+                <TeamOutlined /> Group Management
+              </span>
+            }
+            key="1"
+          >
+            <Group />
+          </TabPane>
+
+          <TabPane
+            tab={
+              <span>
+                <UserOutlined /> Member Management
+              </span>
+            }
+            key="2"
+          >
+            <Member />
+          </TabPane>
+        </Tabs>
+    </div>
   )
 }
 
